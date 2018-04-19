481660572a5f383bbc0b7b385d39fe194d4cfdc2
kennyjeffris <kennyjeffris@gmail.com>
2018-04-18 21:53:55 -0700

    updated arduino

--- ArduinoComm/ArduinoComm.ino
+++ ArduinoComm/ArduinoComm.ino
@@ -1,57 +1,21 @@
-int EMA_S = 0;
-int EMA_a = 0.3;
-int EMA_S1 = 0;
-unsigned long timer = 0;
-long loopTime = 5000;   // microseconds
- 
 void setup() {
+  // Sample at a rate of 9600 Samples/Sec (9.6 kHz)
   Serial.begin(9600);
-  timer = micros();
 }
  
 void loop() {
-  //timeSync(loopTime);
-  int val = analogRead(0);// - 512;
-  //double val = (analogRead(A0) -512) / 512.0;
-  //EMA_S = (EMA_a*val) + ((1-EMA_a)*EMA_S);
-  //int highpass = val - EMA_S;
+  // Get Sensor Values from the respective Sensors
+  int right = analogRead(A0);
+  int left = analogRead(A1);
+  // Convert to sendable packets
+  sendToPC(&right);
+  sendToPc(&left);
 
-  //double val1 = (analogRead(A1) -512) / 512.0;
-  //EMA_S1 = (EMA_a*val) + ((1-EMA_a)*EMA_S1);
-  //int highpass1 = val1 - EMA_S1;
-  Serial.println(val);
-  sendToPC(&val);
-  //sendToPC(&highpass1);
-}
- 
-void timeSync(unsigned long deltaT)
-{
-  unsigned long currTime = micros();
-  long timeToDelay = deltaT - (currTime - timer);
-  if (timeToDelay > 5000)
-  {
-    delay(timeToDelay / 1000);
-    delayMicroseconds(timeToDelay % 1000);
-  }
-  else if (timeToDelay > 0)
-  {
-    delayMicroseconds(timeToDelay);
-  }
-  else
-  {
-      // timeToDelay is negative so we start immediately
-  }
-  timer = currTime + timeToDelay;
 }
  
 void sendToPC(int* data)
 {
   byte* byteData = (byte*)(data);
+  // Send to the Serial Port to communicate with Python
   Serial.write(byteData, 2);
-}
- 
-void sendToPC(double* data)
-{
-  byte* byteData = (byte*)(data);
-  Serial.write(byteData, 4);
 }
